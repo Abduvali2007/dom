3fe3617f7d7d3c3e7c2bb2282834bc6f4adab0ca
abduvali2007 <147902473+Abduvali2007@users.noreply.github.com>
2024-03-10 16:25:33 -0300

    hello

---
+++ script.js
@@ -0,0 +1,30 @@
+let img = document.querySelector(".header-img")
+let text =document.querySelector("h1")
+let header=document.querySelector(".header")
+let img2 =document.querySelector(".camera")
+let img3 =document.querySelector(".heart")
+let img4 =document.querySelector(".person")
+let cub=document.querySelector(".div")
+let btn=document.querySelector("button")
+
+img.style.width ="250px"
+header.style.background="green"
+header.style.display="flex"
+header.style.gap="30px"
+header.style.flexDirection="column"
+header.style.paddingLeft="90px"
+header.style.width="500px"
+header.style.height="750px"
+text.style.marginTop="30px"
+img2.style.width="50px"
+img3.style.width="50px"
+img4.style.width="50px"
+cub.style.display="flex"
+cub.style.gap="60px"
+btn.style.color="white"
+btn.style.fontSize="30px"
+btn.style.borderRadius="10px  "
+btn.style.background="purple"
+btn.style.padding="15px 15px"
+btn.style.width="300px"
+let arr
